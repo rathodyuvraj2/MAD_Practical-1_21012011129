98bbaad5b277dee7dc290036b6ce701f337bbeba
student <student@gnu.ac.in>
2023-07-14 08:28:26 +0530

    Created Practical-1.1

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,21 @@
+fun main(){
+  //  println("Hello world")
+    var a:Int = 10
+    println("$a is integer")
+    var b:Char = 'y'
+    println("$b is character")
+    var c:Float  = 10.5f
+    println("$c is float")
+    var d:Long = 1234564
+    println("$d is long")
+    var e:String = "Yuvraj"
+    println("$e is string")
+    var f:Double = 125.654
+    println("$f is double")
+    var g:Boolean = true
+    println("$g is boolean")
+    var h:Byte = 110
+    println("$h is byte")
+    var i:Short = 910
+    println("$i is Short ")
+}
