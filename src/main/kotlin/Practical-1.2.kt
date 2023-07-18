3ace17e022f2cb81a3386a15c589a96ebcc8eea5
student <student@gnu.ac.in>
2023-07-18 08:23:10 +0530

    created Practical-1.2.kt

---
+++ src/main/kotlin/Practical-1.2.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    var a:Int = 10
+    println("Integer value :$a")
+    var b = a.toString()
+    println("string from integer : $b")
+    var c = a.toDouble()
+    println("double from integer : $c")
+    var d : Double = 10.5
+    println("Double value : $d")
+    var e = d.toInt()
+    println("int value from double : $e" )
+    var f =d.toFloat()
+    println("float value from double : $f")
+
+
+
+}
